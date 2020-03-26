dd46d44316721a57bd02770854b575fa2631930d
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-26 14:25:39 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,11 @@ end
 
 def print_first_directors_movie_titles
 row_index = 0
-while row_index < directors_database.length do
-  p directors_database
- end
- end
+while row_index < spice_rack.count do
+  element_index = 0
+  while element_index < spice_rack[row_index].count do
+    puts spice_rack[row_index][element_index]
+    element_index += 1
+  end
+  row_index += 1
+end
