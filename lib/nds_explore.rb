434212f0008324fa71f3a9f1279954d698205f70
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-26 14:37:33 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,6 +9,21 @@ pp nds
 end
 
 def print_first_directors_movie_titles
-  if directors_database[:name] == "Steven Spielberg"
-    puts directors_database[:name][:movies][:title] 
+outer_results = []
+row_index = 0
+while row_index < directors_database.count do
+  element_index = 0
+  inner_results = []
+  while element_index < directors_database[row_index].count do
+    # How to read the following line of code:
+    #   Array at row_index
+    #   Element of the inner array at element_index
+    #   The first character of that element...
+    if spice_rack[row_index][element_index][0] == "P"
+      inner_results << spice_rack[row_index][element_index]
+    end
+    element_index += 1
+  end
+  outer_results << inner_results
+  row_index += 1
 end
