6f1ca2f3693cc628f96d359b1aead9f1d8fd355e
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-03-26 14:31:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,19 +9,4 @@ pp nds
 end
 
 def print_first_directors_movie_titles
-outer_results = []
-row_index = 0
-while row_index < directors_database.count do
-  element_index = 0
-  inner_results = []
-  while element_index < directors_database[row_index].count do
-    if directors_database[row_index][element_index][0] == "Steven Spielberg"
-      inner_results << directors_database[row_index][element_index]
-    end
-    element_index += 1
-  end
-  outer_results << inner_results
-  row_index += 1
-end
-outer_results
 end
